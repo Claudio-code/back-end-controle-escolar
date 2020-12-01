a8227e4d220bb576612a681afa3def86b63cb3c9
claudio-code <suporte2424@outlook.com>
2020-11-30 23:46:31 -0300

    make exceptions and controllers for entities

---
+++ src/Exception/MatriculationException.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Exception;
+
+use Exception;
+use Throwable;
+
+class MatriculationException extends Exception
+{
+    public function __construct(string $message = '', int $code = 500, Throwable $throwable = null)
+    {
+        parent::__construct("MatriculationException: {$message}", $code, $throwable);
+    }
+
+    public function __toString()
+    {
+        return __CLASS__.": {$this->code}: {$this->message}";
+    }
+}
---
+++ src/Form/CourseType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Course;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CourseType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('description')
+            ->add('totalAmountHours')
+            ->add('created_at')
+            ->add('updated_at')
+            ->add('diciplines')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Course::class,
+        ]);
+    }
+}
